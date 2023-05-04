775a894601d2662f01cbd6a9c0fb0f09a72376e2
Jiří Brhel <jiri.brhel@gmail.com>
2023-05-04 10:11:07 +0200

    Add computed property „Full Title“

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,8 @@
 
 namespace App\Providers;
 
+use Statamic\Facades\Collection;
 use Illuminate\Support\ServiceProvider;
-use Statamic\Statamic;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -24,6 +24,14 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+
+        Collection::computed('timeline', 'full_title', function ($entry, $value) {
+            $instance = $entry->augmentedValue('cities');
+            if (!is_null($entry->id())) {
+                return $entry->get('title') . ' – ' . $instance->value()->title();
+            }
+            return;
+        });
         // Statamic::script('app', 'cp');
         // Statamic::style('app', 'cp');
     }
