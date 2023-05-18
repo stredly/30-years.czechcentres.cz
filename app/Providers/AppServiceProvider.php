b5bb5b3945aecab24aeaaee2cda1172eaf0afe93
Jiří Brhel <jiri.brhel@gmail.com>
2023-05-18 16:23:48 +0200

    Add bard image mutator

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Statamic\Facades\Collection;
 use Illuminate\Support\ServiceProvider;
+use JackSleight\StatamicBardMutator\Facades\Mutator;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -24,6 +25,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        Mutator::html('image', function ($value) {
+            return ['content' => view('partials/_image', $value[1])];
+        });
         // Statamic::script('app', 'cp');
         // Statamic::style('app', 'cp');
     }
